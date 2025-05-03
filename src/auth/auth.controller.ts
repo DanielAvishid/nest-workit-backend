1cd560d0734e8101cec5c4291c33601ae71fbc2e
Daniel Avishid <141136256+DanielAvishid@users.noreply.github.com>
2025-05-03 16:29:47 +0300

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -44,12 +44,9 @@ export class AuthController {
       const { access_token } = await this.authService.login(user);
       
       response.cookie('loginToken', access_token, {
-  httpOnly: true,
-  secure: process.env.NODE_ENV === 'production', // cookie only sent over HTTPS in production
-  sameSite: 'none', // allow cross-origin requests
-  path: '/',
-  maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
-});
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', // cookie only sent over HTTPS in production
+      });
       
       return user;
     } catch (error) {
